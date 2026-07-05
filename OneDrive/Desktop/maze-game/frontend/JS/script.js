06c77eeced82eede43e139a8471e06d416826958
Sevitha <you@gmail.com>
2026-07-05 11:19:33 +0530

    Update frontend files

--- OneDrive/Desktop/maze-game/frontend/JS/script.js
+++ OneDrive/Desktop/maze-game/frontend/JS/script.js
@@ -3,18 +3,8 @@ window.onload = function () {
     setTimeout(function () {
 
         document.getElementById("splash").style.display = "none";
-
         document.getElementById("main").style.display = "flex";
 
-    }, 3000);
+    },3000);
 
 };
-
-document.getElementById("startBtn").addEventListener("click", function () {
-
-    alert("Game Starting...");
-
-    // Later you can redirect:
-    // window.location.href = "maze.html";
-
-});
