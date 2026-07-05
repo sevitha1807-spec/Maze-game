49f6be7b38d72590a778a4c719e4c8e5f641e48b
Sevitha <you@gmail.com>
2026-07-05 12:02:00 +0530

    Fix login button redirect - move listener inside window.onload

--- OneDrive/Desktop/maze-game/frontend/JS/script.js
+++ OneDrive/Desktop/maze-game/frontend/JS/script.js
@@ -5,12 +5,12 @@ window.onload = function () {
         document.getElementById("splash").style.display = "none";
         document.getElementById("main").style.display = "flex";
 
-    },3000);
+    }, 3000);
+
+    document.getElementById("loginBtn").addEventListener("click", function () {
+
+        window.location.href = "login.html";
+
+    });
 
 };
-
-document.getElementById("loginBtn").addEventListener("click", function () {
-
-    window.location.href = "login.html";
-
-});
